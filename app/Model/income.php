006597c01c11e0d5330ab6b2cb33024595985e03
Shakil Mahmud <shakil@bluetechbd.com>
2020-03-03 17:54:00 +0600

    something before passport

--- app/Model/income.php
+++ app/Model/income.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class income extends Model
 {
+    protected $guarded = [];
     //
 }
--- app/Model/loan.php
+++ app/Model/loan.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class loan extends Model
 {
+    protected $guarded = [];
     //
 }
