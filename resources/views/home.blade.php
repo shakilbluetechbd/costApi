8f7f7ec6c4c48a4d881d50c8e08365862b535523
Md. Shakil Mahmud <shakilssac@gmail.com>
2020-03-09 02:08:09 +0600

    client view added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,12 @@
                     @endif
 
                     You are logged in!
+
+                    <div id='app'>
+                        <passport-clients></passport-clients>
+                        <passport-authorized-clients></passport-authorized-clients>
+                        <passport-personal-access-tokens></passport-personal-access-tokens>
+                    </div>
                 </div>
             </div>
         </div>
