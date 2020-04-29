b4184aca0a2aebaf6cae5bb6b7a0edae7b1ad94d
Shakil Mahmud <shakil@bluetechbd.com>
2020-04-30 02:07:20 +0600

    cors problem fixed final

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -16,8 +16,8 @@ class cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Acces-Control-Allow-Origin','*')
-        ->header('Acces-Control-Allow-Methods','PUT,POST,DELETE,GET,OPTIONS')
-        ->header('Acces-Control-Allow-Headers','Accept,Authorization,Content-Type');
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTIONS")
+            ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
     }
 }
